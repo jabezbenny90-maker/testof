309e4f56ec739dcdefffd9ac2850ae776cb9651b
jabezbenny90-maker <jabezbenny90@gmail.com>
2025-12-15 15:03:15 +0530

    addded new json file

---
+++ newfile.js
@@ -0,0 +1,11 @@
+// A simple JavaScript program
+console.log("Hello, World!");
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Example usage
+const result = add(5, 3);
+console.log("The sum is:", result);
